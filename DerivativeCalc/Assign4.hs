77fd67f12092314ea370903c9e0f88d70eee10c4
SophShan <100741746+SophShan@users.noreply.github.com>
2023-01-26 16:52:08 -0500

    Update Assign4.hs

--- DerivativeCalc/Assign4.hs
+++ DerivativeCalc/Assign4.hs
@@ -1,5 +1,5 @@
 {-|
-Module      : 1JC3-Assign3.Assign_3.hs
+Module      : 1JC3-Assign4.Assign_4.hs
 Copyright   :  (c) Curtis D'Alves 2021
 License     :  GPL (see the LICENSE file)
 Maintainer  :  none
@@ -7,225 +7,155 @@ Stability   :  experimental
 Portability :  portable
 
 Description:
-  Assignment 3 - McMaster CS 1JC3 2021
+  Assignment 4 - McMaster CS 1JC3 2021
 -}
 
-{- -----------------------------------------------------------------
- - Datatypes
- - -----------------------------------------------------------------
+{- --------------------------------------------------------------------
+ - Datatype: MathExpr
+ - --------------------------------------------------------------------
+ - Description: An Abstract Syntax Tree (AST) for encoding mathematical
+ -              expressions
+ - Example: The expression
+ -                (abs (2*X + 1)) ^ 3
+ -          can be encoded as
+ -                Power 3 (Func1 Abs
+ -                              (Func2 Add (Func2 Mult (Coef 2) X)
+ -                                         (Coef 1)))
+ - --------------------------------------------------------------------
  -}
-newtype Graph a = Graph [(Node a,[NodeID])]
-  deriving (Show,Eq)
-
-type NodeID = Int
-
-data Node a = Node { getNodeID  :: NodeID,
-                     getNodeVal :: a }
-  deriving (Show,Eq,Ord)
-
-
-{- -----------------------------------------------------------------
- - Example Graph
- - -----------------------------------------------------------------
- -              -----        ----------
- -              | A |------->| C |    |
- -              -----        ----- <---
- -                |           |
- -                |     ------|
- -                v     |
- -              ----- <-|
- -              | B |
- -              -----
- -}
-nodeA,nodeB,nodeC :: Node Char
-nodeA = Node 0 'A'
-nodeB = Node 1 'B'
-nodeC = Node 2 'C'
-
-exGraph :: Graph Char
-exGraph = Graph [(nodeA,[1,2])
-                ,(nodeB,[])
-                ,(nodeC,[1,2])]
-
-
-
+data MathExpr a =
+    X
+  | Coef a
+  | Add (MathExpr a) (MathExpr a)
+  | Mult (MathExpr a) (MathExpr a)
+  | Power (MathExpr a) Int
+  | Cos (MathExpr a)
+  | Sin (MathExpr a)
+  | Abs (MathExpr a)
+  deriving (Eq,Show,Read)
 
 {- -----------------------------------------------------------------
- - maxNodeID
+ - eval
  - -----------------------------------------------------------------
  - Description:
- -    This function takes in an input of type Graph a and returns the largest NodeID in the graph as type Maybe NodeID. If the input given is an empty 
- Graph, the function returns Nothing. Otherwise, the first NodeID in the Graph (NodeId of the head of the Graph) and the remaining elements of the Graph 
- are passed as parameters to the function maxAux, which returns the NodeID with the maximum value.
+ -    Takes in as input an expression of type MathExpr and a float or int type (if int type is imputed, it will be used as a float). Eval then evaluates 
+ the MathExpr at the given float and outputs the value. If solely X is entered, the inputted float is outputted. If a Coef is inputted, the value of Coef 
+ is outputted. These are the two base cases on which the remaining pattern matching cases (which utilize recursion), rely on. The output of this function 
+ is a float.
  -}
-maxNodeID :: Graph a -> Maybe NodeID
-maxNodeID  (Graph []) = Nothing
-maxNodeID  (Graph xs) = 
-     let y = Just (getNodeID (fst (head xs)))
-     in maxAux (Graph (tail xs)) y
+eval :: (Floating a, Eq a) => MathExpr a -> a -> a
+eval X v = v
+eval (Coef x) _ = x
+eval (Add x y) v = eval x v + eval y v
+eval (Mult x y) v = eval x v * eval y v
+eval (Power x y) v = (eval x v) ^^ y
+eval (Cos x) v = cos (eval x v)
+eval (Sin x) v = sin (eval x v)
+eval (Abs x) v = abs (eval x v)
 
+{- -----------------------------------------------------------------
+ - instance Num a => Num (MathExpr a)
+ - -----------------------------------------------------------------
+ - Description:
+ -    These implementations for the below methods allow one to use the methods on the custom data type MathExpr by defining each method in terms of 
+ MathExpr. This allows inputs of Num (MathExpr a) to be used with the methods.  
+ -}
+instance Num a => Num (MathExpr a) where
+  x + y         = Add x y
+  x * y         = Mult x y
+  negate x      = Mult (Coef (-1)) x
+  abs x         = Abs x
+  fromInteger i = Coef (fromInteger i)
+  signum _      = error "signum is left un-implemented"
 
-{-  -----------------------------------------------------------------
- - maxAux
+{- -----------------------------------------------------------------
+ - instance Fractional a => Fractional (MathExpr a)
  - -----------------------------------------------------------------
  - Description: 
- -     This function takes an input of type Graph a and a MaybeNodeID and returns the NodeID of the node with the maximum value (of all the Nodes in the 
- Graph as well as n) as a Maybe NodeID. If the input of the Graph type is an empty Graph, it returns n as the max value as it is the only value to compare. Else, it compares n to the value of the head of the Graph. If the value of the head of the graph is greater, maxAux is called again with the tail of the graph and the NodeID of the head as n. Otherwise, maxAux is called again with the same input n and the tail of the graph.
+-      These implementations for the below methods allow one to use the methods on the custom data type MathExpr by redefining each method in terms of 
+MathExpr. These instances allow inputs of Fractional (MathExpr a) to be used with the below methods.
+ -    
+ -}
+instance Fractional a => Fractional (MathExpr a) where
+  recip e        = Power e (-1)
+  fromRational e = Coef (fromRational e)
 
--}
-maxAux :: Graph a -> Maybe NodeID -> Maybe NodeID
-maxAux (Graph []) n = n
-maxAux (Graph xs) n  
-  | headNodeID > n = maxAux (Graph (tail xs)) (headNodeID)
-  | otherwise = maxAux (Graph (tail xs)) n
-  where
-     headNodeID = Just (getNodeID (fst (head xs)))
+{- -----------------------------------------------------------------
+ - instance Floating a => Floating (MathExpr a)
+ - -----------------------------------------------------------------
+ - Description:
+ -    This defines instances of the below methods in terms of the custom data type MathExpr a and allows input of Floating (MathExpr a) to be used with the
+ methods.
+ -}
+instance Floating a => Floating (MathExpr a) where
+  pi      = Coef pi
+  sin x   = Sin x
+  cos x   = Cos x
+  log     = error "log is left un-implemented"
+  asin _  = error "asin is left un-implemented"
+  acos _  = error "acos is left un-implemented"
+  atan _  = error "atan is left un-implemented"
+  sinh _  = error "sinh is left un-implemented"
+  cosh _  = error "cosh is left un-implemented"
+  tanh _  = error "tanh is left un-implemented"
+  asinh _ = error "asinh is left un-implemented"
+  acosh _ = error "acosh is left un-implemented"
+  atanh _ = error "atanh is left un-implemented"
+  exp _   = error "exp is left un-implemented"
+  sqrt _  = error "sqrt is left un-implemented"
+
+{- -----------------------------------------------------------------
+ - diff
+ - -----------------------------------------------------------------
+ - Description:
+ -    This function takes in an input of MathExpr a and preforms pattern matching on the constructors to differentiate the MathExpr accodring to 
+ differentiation rules. The differentiated MathExpr is then outputted as type MathExpr.
+ -}
+diff :: (Floating a, Eq a) => MathExpr a -> MathExpr a
+diff X = Coef 1
+diff (Coef x) = Coef 0
+diff (Add x y) = Add (diff x) (diff y)
+diff (Mult x y) = Add (Mult (diff x) y) (Mult x (diff y))
+diff (Power x y) = Mult (Mult (diff x) (Coef (fromIntegral y))) (Power x (fromIntegral y-1))
+diff (Cos x) = Mult (diff x)  (Mult (Coef (-1)) (Sin x))
+diff (Sin x) = Mult (Cos x) (diff x)
+diff (Abs x) = Mult (Mult x (recip (abs x))) (diff x)
   
 {- -----------------------------------------------------------------
- - insertNode
+ - pretty
  - -----------------------------------------------------------------
- - Description:
- -    This function takes in an input of any type a as the value for the to be inserted node, and type Graph a. This function inserts a new Node to the 
- Graph given, with its value as the value inputed (v). The NodeID of the new Node is one greater than the greatest NodeID value in the inputted Graph 
- unless the Graph is empty, in which case its NodeID is 0. This function calls on maxNodeID to get the maximum NodeID of the Graph (when it is not empty), 
- and since the output of maxNodeID is Maybe type, and type a was needed to concatenate the new node to the rest of the list of Nodes, the output was passed
- to the function auxMaybeConvert to convert it from Maybe type to type a.
- -}
-insertNode :: a -> Graph a -> Graph a
-insertNode v (Graph []) = Graph [(Node 0 v,[])]
-insertNode v (Graph xs) = 
-  let maxNodeIDvalue = auxMaybeConvert (maxNodeID (Graph xs))
-  in Graph (xs ++ [(Node (maxNodeIDvalue + 1) v, [])])
+ - Description: This function takes in a type MathExpr and pattern matches it according to its constructor to output the String representation of the 
+ MathExpr.
 
-{- -----------------------------------------------------------------
- - auxMaybeConvert
- - -----------------------------------------------------------------
- - Description:
- -    This function takes in a type Maybe int and removes its "wrapper" Just, returning a value of type a that can then be used in the function insertNode.
  -}
-auxMaybeConvert :: Maybe a -> a
-auxMaybeConvert (Just a) = a
-
-{- -----------------------------------------------------------------
- - removeNode
- - -----------------------------------------------------------------
- - Description:
- -    This function takes as input a NodeID and type Graph a, removes the Node from the graph with the matching NodeID, as well as its NodeID from any of 
- the edges of the Graph, and returns a new Graph of type Graph a. If the Graph inputed is empty, the function returns an empty Graph. Else, the function 
- calls on an auxilary function auxRemove to preform the previously mentioned operations and returns the result of the auxRemove as a Graph. 
- -}
-removeNode :: NodeID -> Graph a -> Graph a
-removeNode nID (Graph []) = Graph []
-removeNode nID (Graph xs) = Graph (auxRemove nID (Graph xs))
-  
-{- -----------------------------------------------------------------
- - auxRemove
- - -----------------------------------------------------------------
- - Description:
- -    This function takes as input a NodeId and Graph a. The function returns type [(Node a, [NodeID])] which is the same type as a Node, but without the
- "wrapper" to label it as a Graph. If the Graph inputted is empty, the function returns an empty list. Otherwise, it check is see if the NodeID of the 
- first Node in the Graph {variable nodeID which = getNodeID (fst (head xs))} matches with the given NodeID. If it does, it skips over that element but 
- continues to traverse through the rest of the Graph nodes to remove all edges of the same NodeID. If the NodeId do not match, it returns the first node of
- the graph with any edges containing the inputted NodeId removed and combines it with another call of the function auxRemove on the tail of the Graph.
- -}
-auxRemove :: NodeID -> Graph a -> [(Node a, [NodeID])]
-auxRemove nID (Graph []) = []
-auxRemove nID (Graph xs) 
-  | nID == nodeID = auxRemove nID (Graph (tail xs)) 
-  | otherwise = (Node nodeID nodeVal, auxDelEdge nID nodeList) : (auxRemove nID (Graph (tail xs))) 
-   where 
-      nodeID = getNodeID (fst (head xs))
-      nodeVal = getNodeVal (fst (head xs))
-      nodeList = snd (head xs)
-
-{- -----------------------------------------------------------------
- - auxDelEdge
- - -----------------------------------------------------------------
- - Description:
- -    This function takes in a NodeID (int) and a list of NodeID ([int]) and returns the list of NodeId with the given NodeId removed. If an empty list is 
- imputted, an empty list will be returned. Else, if the head of the list matches the nodeID given (nID), then the remaining list is returned. Otherwise if 
- the head of the list does not match, the head is concatenated with another call of the function auxDelEdge (with the same NodeID and the tail of the list 
- as input) and is returned.
- -}
-auxDelEdge :: NodeID -> [NodeID] -> [NodeID]
-auxDelEdge nID [] = []
-auxDelEdge nID xs 
-  | head xs == nID = tail xs
-  | otherwise = head xs : auxDelEdge nID (tail xs)
+-- NOTE: you'll have to test pretty yourself
+pretty :: (Show a) => MathExpr a -> String
+pretty X = "X"
+pretty (Coef x) = "(" ++ show x ++ ")"
+pretty (Add x y) = pretty x ++ " + " ++ pretty y
+pretty (Mult x y) =  pretty x ++ " * " ++ pretty y
+pretty (Power x y) = pretty x ++ " ^^ " ++ pretty (Coef y)
+pretty (Cos x) = "cos(" ++ pretty x ++ ")"
+pretty (Sin x) = "sin(" ++ pretty x ++ ")"
+pretty (Abs x) = "abs(" ++ pretty x ++ ")"
 
 
 {- -----------------------------------------------------------------
- - lookupNode
+ - Test Cases
  - -----------------------------------------------------------------
- - Description:
- -    This function taks as an input a NodeID and Graph a. It checks to see if the head Node in the inputted Graph a (where the node = fst(headxs)) has the
- same NodeId as the NodeID inputted. If there is such as Node, that Node is returned as a Maybe type. Otherwise, the function lookupNode is called again 
- with the same NodeId as input and the tail of the Graph. If an empty Graph is inputted, Nothing is returned.
+ -}
+infix 4 =~
+(=~) :: (Floating a,Ord a) => a -> a -> Bool
+x =~ y = abs (x - y) <= 1e-4
+
+{- EXAMPLE
+ - Function: eval
+ - Property: eval (Func2 Add (Coef x) X) y is correct for all x,y
+ - Actual Test Result: Pass
  -}
 
-lookupNode :: NodeID -> Graph a -> Maybe (Node a)
-lookupNode nID (Graph []) = Nothing 
-lookupNode nID (Graph xs) 
-  |getNodeID node == nID = Just node
-  |otherwise = lookupNode nID (Graph (tail xs))
-  where 
-    node = fst (head xs)
+evalProp0 :: (Float,Float) -> Bool
+evalProp0 (x,y) = (x + y) =~ eval (Add (Coef x) X) y
 
-
-{- -----------------------------------------------------------------
- - insertEdge
- - -----------------------------------------------------------------
- - Description:
- -   This function takes in a tuple containing two NodeID and a Graph a (of any type). The function calls on the function containsBothNodes to check if 
- both NodeIds exist in the graph. This is done by calling on the function lookupNode with each of the NodeIDs in the tuple. If either of these calls 
- return Nothing, insertEdge will return Nothing. Otherwise, the function calls on the auxilary function, auxInsert to insert the NodeId of the second 
- element in the tuple into the list of edges for the Node matching the NodeID of the first element in the tuple. insertEdge returns a new Graph of type 
- Maybe with the Node matching the first NodeID in the tuple containing the second NodeID of the tuple as one of the elements of its edges.
- -}
-insertEdge :: Eq a => (NodeID,NodeID) -> Graph a -> Maybe (Graph a)
-insertEdge _       (Graph [])  = Nothing
-insertEdge (n1,n2) g@(Graph graph)
-  | not (containsBothNodes n1 n2) = Nothing
-  | otherwise = Just (Graph (auxInsert n1 n2 (Graph graph)))
-  where
-    containsBothNodes :: NodeID -> NodeID -> Bool
-    containsBothNodes n1 n2
-      | lookupNode n1 (Graph graph) == Nothing = False
-      | lookupNode n2 (Graph graph) == Nothing = False
-      | otherwise = True
-
-{- -----------------------------------------------------------------
- - auxInsert
- - -----------------------------------------------------------------
- - Description:
- -   This function takes in two NodeIds and a Graph a. If the Graph inputted is empty, an empty Graph is returned. Else, the function traverses through the
- Graph to find the Node that matches the first NodeID (by comparing the first NodeId to the NodeId of the head Node of the graph). When it finds the Node, 
- it concantenates the node with the function checkDup called on its list of edges, to the remaining list of Nodes and returns this result as type [(Node a,
- [NodeID])] (which is the same type as a Node).
- -}
-auxInsert :: NodeID -> NodeID -> Graph a -> [(Node a, [NodeID])]
-auxInsert n1 n2 (Graph []) = []
-auxInsert n1 n2 (Graph xs) 
-   |nodeID == n1 = (Node nodeID nodeVal, checkDup nodeList n2) : tail xs
-   |otherwise = head xs : auxInsert n1 n2 (Graph (tail xs))
-  where 
-   nodeID = getNodeID (fst (head xs))
-   nodeVal = getNodeVal (fst (head xs))
-   nodeList = snd (head xs)
-
-{- -----------------------------------------------------------------
- - checkDup
- - -----------------------------------------------------------------
- - Description:
- -   This function takes as an input a list of NodeID and a NodeID. If the list is empty, it returns a list with the sole NodeID (adds the edge to the 
- list). Otherwise it traverses the list to check if any of the elements are the same as the the given NodeID (so that no duplicates will be added.) This is
- done by checking head of list against the NodeID. If they match, the list unchanged is returned. If they do not match, the head of the list is 
- concatenated with another call of the function checkDup on the tail of the list with the original NodeID value. This way, if the edge already exists in 
- the list, a duplicate will not be added and the list will be returned unchanged. Otherwise, if the whole list is tranvered and the element is not found, 
- the edge is added to the end of the list and returned.
- -}
-checkDup :: [NodeID] -> NodeID -> [NodeID]
-checkDup [] n2 = [n2]
-checkDup xs n2
-   | head xs == n2 = xs
-   | otherwise = head xs : checkDup (tail xs) n2
+runEvalProp0 :: IO ()
+runEvalProp0 = quickCheck  evalProp0
